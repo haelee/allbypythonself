fc9e0815fd55d60f5c9f70e11c042b990e4a1ed1
Hae Young Lee <whichmeans@gmail.com>
2019-09-26 12:33:58 +0900

    Update arpspoof.py

--- arpspoof.py
+++ arpspoof.py
@@ -1,7 +1,7 @@
-# all-by-pythonself
-# by whichmeans
-# at Cheongju University
+# All-by-Pythonself
 # Snippet for ARP cachie poisoning
+# by Hae Young Lee
+# at Cheongju University
 
 from scapy . all import *
 
