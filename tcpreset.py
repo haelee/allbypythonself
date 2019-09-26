0aab25d6db1054663621095581c66a49fe4b9764
Hae Young Lee <whichmeans@gmail.com>
2019-09-26 12:36:11 +0900

    Update tcpreset.py

--- tcpreset.py
+++ tcpreset.py
@@ -1,7 +1,7 @@
-# all-by-pythonself
-# by whichmeans
-# at Cheongju University
+# All-by-Pythonself
 # Snippet for TCP reset attacks
+# by Hae Young Lee
+# at Cheongju University
 
 from scapy . all import *
 
